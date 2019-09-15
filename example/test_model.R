59e512c215d57f1daab74203700e6cd49167f23f
QWERTZ11 <caizeqing@outlook.com>
2019-09-15 15:52:12 +0200

    structure of files

--- example/test_model.R
+++ example/test_model.R
@@ -7,11 +7,5 @@ learner =regr.lrn = makeLearner("regr.gbm", par.vals = list(n.trees = 500, inter
 mod.mlr = train(learner, task)
 PrediObj = Predictor$new(mod.mlr, data = BostonHousing)
 saveRDS(PrediObj, file = "PrediObj.RDS")
-X = as.data.frame(PrediObj$data$get.x())
-x.interest = X[6,]
-model_data = Predictor$new(PrediObj$model, data = X)
-set.seed(seed = seed)
-shapley = Shapley$new(predictor = model_data, x.interest = x.interest, sample.size = 100)
-plot(shapley)
-shapley$results[,c(1, 4, 2)]
+
 
